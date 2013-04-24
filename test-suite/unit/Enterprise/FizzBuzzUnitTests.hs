599a4041382e110e581fe802e458055fa5811d15
JHawk <josh.r.hawk@gmail.com>
2013-04-23 21:20:29 -0400

    minor changes

--- test-suite/unit/Enterprise/FizzBuzzUnitTests.hs
+++ test-suite/unit/Enterprise/FizzBuzzUnitTests.hs
@@ -8,16 +8,16 @@ main = defaultMain fizzBuzzUnitTests
 
 fizzBuzzUnitTests = 
   [
-    testGroup "describing fizzbuzz" [
-      testCase "should fail" on_purpose,
+    testGroup "showing a fizzbuzz fail" [
+      testCase "should fail" on_purpose
+    ],
+    testGroup "showing fizzbuzz pass" [
       testCase "should return 1" one_is_entered,
       testCase "should return Fizz" three_is_entered
-    ]
+    ] 
   ]
 
 on_purpose = fizzBuzz 10 @?= "not here"
-
 one_is_entered = fizzBuzz 1 @?= "1"
-
 three_is_entered = fizzBuzz 3 @?= "Fizz"
 
